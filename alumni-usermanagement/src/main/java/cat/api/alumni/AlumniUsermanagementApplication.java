a96fbdfb62d0b8f4ff1f7fa0ce64531d238ae4a7
Pepe Ramos <jramma@MacBook-Air-de-Pepe.local>
2023-08-16 16:16:52 +0200

    usermanagement

---
+++ alumni-usermanagement/src/main/java/cat/api/alumni/AlumniUsermanagementApplication.java
@@ -0,0 +1,13 @@
+package cat.api.alumni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumniUsermanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumniUsermanagementApplication.class, args);
+	}
+
+}
---
+++ alumni-usermanagement/src/test/java/cat/api/alumni/AlumniUsermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.api.alumni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumniUsermanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
