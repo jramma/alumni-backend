56c0271bc856b92ae9d2f2bbe7d11c9adf8b59d4
Pepe Ramos <jramma@MacBook-Air-de-Pepe.local>
2023-08-16 12:14:42 +0200

    login

---
+++ psico-service/src/main/java/cat/api/study/PsicoServiceApplication.java
@@ -0,0 +1,13 @@
+package cat.api.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsicoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsicoServiceApplication.class, args);
+	}
+
+}
---
+++ psico-service/src/test/java/cat/api/study/PsicoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.api.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsicoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
